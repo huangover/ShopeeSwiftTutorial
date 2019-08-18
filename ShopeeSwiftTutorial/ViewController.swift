9da04eb4bb5df0267a4617b74b13088cfc2415e3
Sihang Huang <sihang@SHMBP.local>
2019-08-18 15:31:58 +0800

    第一步

--- ShopeeSwiftTutorial/ViewController.swift
+++ ShopeeSwiftTutorial/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let tableView: UITableView
+    
+    init() {
+        tableView = UITableView();
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        tableView.dataSource = self;
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
 }
 
